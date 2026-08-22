75d3ccc24aec5493a128ab1ee779fc0f33f8c2c7
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-22 11:04:19 +0000

    CHAnges

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 14453
+let accountEmail = "amisha@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur" 
+let accountState;
+// accountID = 2
+accountEmail = "hch@.com"
+accountPassword= "21212121"
+accountCity = "Bengaluru"
+console.log(accountID);
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+
+// alert(3+3) // we are using node js , not browser 
+console.log(3
+    +
+    3) // code readability should be high
+console.log("Amisha")
+
+
+let name = "Amisha"
+let age = 23
+let isLoggedIn = true
+
+
+// number => 2 to pow 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null=> standalone value
+// undefined => 
+//symbol => uniqueness 
+
+
+//object   
+console.log(typeof age);
+console.log(typeof "Amisha")
+console.log(typeof undefined)

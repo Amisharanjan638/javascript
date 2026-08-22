1a50ef8d90bf222301b14901915e7edba55eb7d0
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-22 14:26:42 +0000

    chan

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,65 @@
+/*let score = 33
+
+
+
+let valueINNumber = Number (score)
+console.log(typeof valueINNumber );
+console.log(valueINNumber); */
+
+// "33" => 33
+// "33abc" =>Nan 
+// true => 1 ; false =>0
+
+/*let isLoggedIn = "Amisha"
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn) */
+
+//1 => true
+// 0 => false
+// "" => false
+// "any string" => true
+
+/*let num = 33
+let stringnum = String(num)
+console.log(stringnum);
+console.log(typeof stringnum) */
+
+
+// ****************************Operations ***************
+
+let value = 3
+let negVal = -value
+//console.log(negVal)
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3) */
+
+let str1 = "Hello"
+let str2 = " Amisha"
+let str3 = str1 + str2
+/*console.log(str3)
+
+console.log(1+"2")
+console.log("1" + 2)
+console.log("1" + 2 + 2) */
+
+// console.log(true+0)
+// console.log(+"")
+
+
+
+/*let num1 , num2 , num3;
+num1 = num2 = num3 = 2+2 */
+
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter) 
+

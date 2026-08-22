ea82f906cbba34833e79833b85a58530b699361b
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-22 15:19:00 +0000

    c

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,39 @@
+// primitive 
+
+// 7 types : String ,Number , Boolean ,Null , Undefined , Bigint , Symbol
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+
+const outsideTemp = null
+let userEmail;
+
+const id  = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 333333333333n
+
+//Reference type or Non - primitive 
+
+//Arrays , Objects , Functions
+
+
+
+const heros = ['shaktiman' , "naagraj" , "doga" ]
+let myObj ={
+    name: "Amisha",
+    age:2
+}
+
+const myfunction = function() {
+    console.log("hello world")
+}
+
+// console.log(heros)
+// console.log(myObj)
+// myfunction()
+console.log(typeof heros)
+ console.log(typeof myfunction) 

7083f11d2d7ab246b2d0fe42a595f341658c00f3
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-25 10:42:46 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,7 +4,7 @@ if(true) {
     let a = 10
 const b = 20
 //var c = 30
-console.log("Inner:", a)
+//console.log("Inner:", a)
 }
 
 
@@ -13,6 +13,55 @@ console.log("Inner:", a)
 //}
 
 
-console.log(a)
+//console.log(a)
 //console.log(b)
 //console.log(c)
+
+
+function one() {
+  const username = "Amisha"
+
+  function two (){
+    const website = "youtube"
+    //console.log(username);
+  }
+//  console.log(website)
+  //two()
+}
+
+
+//one()
+
+
+if(true) {
+  const username ="Amisha"
+  if(username==="Amisha") {
+    const website = " youtube"
+  //  console.log(username+ website)
+  }
+
+ // console.log(website)
+
+}
+
+
+//console.log(username)
+
+
+
+//++++++++++++++++++++interesting+++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+
+
+function addone(num) {
+  return num+1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num) {
+  return num + 2
+}

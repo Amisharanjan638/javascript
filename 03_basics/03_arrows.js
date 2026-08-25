475db935390c12645072645bf9994a8a225ef891
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-25 11:37:40 +0000

    arrow

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,61 @@
+const user = {
+    username:"Amisha",
+    price:999,
+    welcomeMessage:function() {
+    //    console.log(`${this.username} , welcome to website`)
+  //   console.log(this)   
+    }
+
+
+}
+
+//user.welcomeMessage()
+//user.username = "sam"
+//user.welcomeMessage() 
+
+//console.log(this)
+
+/*function chai() {
+    let username="khushi"
+    console.log(this)
+}
+
+chai()
+
+
+const chai = function() {
+    let username = "Amisha"
+    console.log(this.username)
+}
+
+chai()*/
+
+
+const chai =()=> {
+    let username = "Amisha"
+    console.log(this)
+}
+
+//chai()
+
+
+//const addTwo =(num1 , num2) =>  num1+num2
+
+
+//const addTwo =(num1,num2) => (num1+num2)
+//console.log(addTwo(5,5))
+
+const addTwo=(num1,num2) => ({username:"Amisha"})
+
+
+console.log(addTwo(3,4)) 
+
+
+//const myArray = [2,3,4,5,6]
+
+//myArray.forEach(()=> {} )
+
+
+
+
+

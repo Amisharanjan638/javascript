2f0c51ae57d18a825ac45007ca6b5a68177446c3
Amisharanjan638 <amisharanjan43@gmail.com>
2026-08-26 04:15:13 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expression(IIFE)
+
+(function chai() {
+    //named iife
+    // console.log("DB CONNECTED")
+
+})();
+
+
+
+
+( (name) => { 
+    console.log(`DB CONNECTION TWO , ${name}`)
+})("Amisha")
